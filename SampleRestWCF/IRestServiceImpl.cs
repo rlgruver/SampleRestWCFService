5b0af6959022239d7dd374cf2a76e48121ccb4f9
rgruver <rgruver@godaddy.com>
2015-07-14 16:39:01 -0700

    first commit

---
+++ SampleRestWCF/IRestServiceImpl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SampleRestWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestServiceImpl" in both code and config file together.
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+              ResponseFormat = WebMessageFormat.Xml,
+              BodyStyle = WebMessageBodyStyle.Wrapped,
+              UriTemplate = "xml/{time}")]
+        string XMLData(int time);
+
+    }
+}
---
+++ SampleRestWCF/RestServiceImpl.svc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+
+namespace SampleRestWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImpl" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImpl.svc or RestServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestService Members
+
+        public string XMLData(int time)
+        {
+            return "You requested to sleep for " + time;
+            Thread.Sleep(time);
+
+        }
+
+        
+
+        #endregion
+    }
+}
